feb792d709b0c96ecd81eecc50dda82dc9f44e30
Konstantin Shelekhin <k.shelekhin@yadro.com>
2024-02-05 23:56:56 +0300

    tests: better wording
    Clarify what tests actually do.

--- tests/test_building.py
+++ tests/test_building.py
@@ -1,6 +1,6 @@
 from scapy_iscsi.iscsi import *
 
-def test_login_request_constructor():
+def test_build_login_request():
     login_parameters = {
         "InitiatorName": "iqn.2023-01.com.example:initiator",
         "TargetName": "iqn.2023-01.com.example:target",
@@ -18,13 +18,11 @@ def test_login_request_constructor():
     }
     pdu = ISCSI() / LoginRequest(ds=kv2text(login_parameters))
     with open("tests/pdu/login-request", 'br') as f:
-       reference = f.read() 
-       assert(reference == pdu.build())
+       assert(f.read() == pdu.build())
 
-def test_scsi_command_constructor():
+def test_build_scsi_command():
     cdb = CDB() / WRITE16(xfer_len=1)
     pdu = ISCSI() / SCSICommand(flags="WF", itt=0x1, cmdsn=0x1, edtl=512,
                                 cdb=cdb, ds=(b'A' * 512))
     with open("tests/pdu/write16", 'br') as f:
-       reference = f.read() 
-       assert(reference == pdu.build())
+       assert(f.read() == pdu.build())
